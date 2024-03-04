a6503048236fe1bbc2ff7d4a4a84f01f9a1432a5
teikv <levanquang@Macintosh.local>
2024-03-04 13:22:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/p4/home_activity.java
@@ -0,0 +1,120 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		home
+	 *	@date 		Monday 04th of March 2024 06:05:12 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.p4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class home_activity extends Activity {
+
+	
+	private View _bg__home_ek2;
+	private View rectangle;
+	private ImageView ellipse;
+	private TextView jane;
+	private TextView san_francisco__ca;
+	private ImageView rectangle_2;
+	private ImageView rectangle_2_5;
+	private ImageView rectangle_2_1;
+	private ImageView rectangle_2_3;
+	private ImageView rectangle_2_4;
+	private ImageView rectangle_2_2;
+	private View _bg__button_ek1;
+	private View rectangle_2_ek1;
+	private TextView see_more;
+	private View _bg__button_ek3;
+	private View rectangle_2_ek2;
+	private TextView follow_jane;
+	private View _bg__button_ek5;
+	private View rectangle_2_ek3;
+	private TextView message;
+	private View _bg__tab_bar_ek1;
+	private View _bg__background_ek1;
+	private View background_ek2;
+	private View _bg__home_ek4;
+	private ImageView union;
+	private View _bg__search_ek1;
+	private ImageView union_ek1;
+	private View _bg__upload_ek1;
+	private View rectangle_ek1;
+	private ImageView union_ek2;
+	private View _bg__chat_ek1;
+	private ImageView union_ek3;
+	private View _bg__profile_ek1;
+	private View _bg__person_ek1;
+	private ImageView ellipse_ek1;
+	private View ellipse_ek2;
+	private View shape;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home);
+
+		
+		_bg__home_ek2 = (View) findViewById(R.id._bg__home_ek2);
+		rectangle = (View) findViewById(R.id.rectangle);
+		ellipse = (ImageView) findViewById(R.id.ellipse);
+		jane = (TextView) findViewById(R.id.jane);
+		san_francisco__ca = (TextView) findViewById(R.id.san_francisco__ca);
+		rectangle_2 = (ImageView) findViewById(R.id.rectangle_2);
+		rectangle_2_5 = (ImageView) findViewById(R.id.rectangle_2_5);
+		rectangle_2_1 = (ImageView) findViewById(R.id.rectangle_2_1);
+		rectangle_2_3 = (ImageView) findViewById(R.id.rectangle_2_3);
+		rectangle_2_4 = (ImageView) findViewById(R.id.rectangle_2_4);
+		rectangle_2_2 = (ImageView) findViewById(R.id.rectangle_2_2);
+		_bg__button_ek1 = (View) findViewById(R.id._bg__button_ek1);
+		rectangle_2_ek1 = (View) findViewById(R.id.rectangle_2_ek1);
+		see_more = (TextView) findViewById(R.id.see_more);
+		_bg__button_ek3 = (View) findViewById(R.id._bg__button_ek3);
+		rectangle_2_ek2 = (View) findViewById(R.id.rectangle_2_ek2);
+		follow_jane = (TextView) findViewById(R.id.follow_jane);
+		_bg__button_ek5 = (View) findViewById(R.id._bg__button_ek5);
+		rectangle_2_ek3 = (View) findViewById(R.id.rectangle_2_ek3);
+		message = (TextView) findViewById(R.id.message);
+		_bg__tab_bar_ek1 = (View) findViewById(R.id._bg__tab_bar_ek1);
+		_bg__background_ek1 = (View) findViewById(R.id._bg__background_ek1);
+		background_ek2 = (View) findViewById(R.id.background_ek2);
+		_bg__home_ek4 = (View) findViewById(R.id._bg__home_ek4);
+		union = (ImageView) findViewById(R.id.union);
+		_bg__search_ek1 = (View) findViewById(R.id._bg__search_ek1);
+		union_ek1 = (ImageView) findViewById(R.id.union_ek1);
+		_bg__upload_ek1 = (View) findViewById(R.id._bg__upload_ek1);
+		rectangle_ek1 = (View) findViewById(R.id.rectangle_ek1);
+		union_ek2 = (ImageView) findViewById(R.id.union_ek2);
+		_bg__chat_ek1 = (View) findViewById(R.id._bg__chat_ek1);
+		union_ek3 = (ImageView) findViewById(R.id.union_ek3);
+		_bg__profile_ek1 = (View) findViewById(R.id._bg__profile_ek1);
+		_bg__person_ek1 = (View) findViewById(R.id._bg__person_ek1);
+		ellipse_ek1 = (ImageView) findViewById(R.id.ellipse_ek1);
+		ellipse_ek2 = (View) findViewById(R.id.ellipse_ek2);
+		shape = (View) findViewById(R.id.shape);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
